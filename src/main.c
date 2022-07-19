3d03d5fafbc450ab6cfc25511d8759926c0eaa63
T-Shai <shaithan.2000@gmail.com>
2022-07-19 15:09:29 +0200

    compare sw and hw + timing

--- src/main.c
+++ src/main.c
@@ -5,7 +5,11 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
-#define THRES 127
+#include "time.h"
+
+#define THRES       127
+#define DATA_SIZE   1000
+
 
 void test1() 
 {
@@ -41,7 +45,8 @@ void test2()
 }
 
 
-void memdump(void* virtual_address, int byte_count) {
+void memdump(void* virtual_address, int byte_count)
+{
     char *p = virtual_address;
     int offset;
     for (offset = 0; offset < byte_count; offset++) {
@@ -51,39 +56,60 @@ void memdump(void* virtual_address, int byte_count) {
     printf("\n");
 }
 
+void fill_random(unsigned int* buff, size_t len)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+        buff[i] = rand()%255;
+    }
+}
+
+
+int compare_arrays(unsigned int* ibuff1, unsigned int* ibuff2, size_t data_size)
+{
+    unsigned char *buff1 =  ibuff1;
+    unsigned char *buff2 =  ibuff2;
+
+    for(size_t i = 0; i < data_size; i++ )
+    {
+        if( buff1[i] != buff2[i]){
+            printf("%u, %02x != %02x \n", i,  buff1[i], buff2[i]);
+            return 0;
+        }
+    }
+    printf("buff1 = buff2\n");
+    
+    return 1;
+}
 void test3()
 {
+    srand(time(NULL));
     // loading bitstream
     load_bitstream("/home/xilinx/pynq/overlays/threshold/threshold.bit");
 
-    // create image as c array
-    sImg* img;
-    load_image(&img, "data/car.pgm");
-    
-
-    const unsigned int data_size = img->width*img->height;
     //getting the status of DMA channels
     unsigned int* dma_mmio = get_mmio(DMA_BASE, REGISTER_SIZE);
     if(!dma_mmio) return;
-    unsigned int* send_buff = get_mmio(DMA_SEND_ADDR, data_size*4);
+    unsigned int* send_buff = get_mmio(DMA_SEND_ADDR, DATA_SIZE*4);
     if(!send_buff) return;
-    unsigned int* recv_buff = get_mmio(DMA_RECV_ADDR, data_size*4);
+    unsigned int* recv_buff = get_mmio(DMA_RECV_ADDR, DATA_SIZE*4);
     if(!recv_buff) return;
 
     unsigned int* ip_mmio = get_mmio(IP_AXI_LITE_BASE, REGISTER_SIZE);
     if(!ip_mmio) return;
 
-    ip_mmio[0] = 0x81;
+
+    /*
+        starting threshold IP trough axi lite :
+            0x0  : offset of the control register
+            0x81 enables bit corresponding to ap_start and auto_restart
+    */
+    ip_mmio[0x0] = 0x81;
     
-    memset(send_buff, 0, data_size);
-    memset(recv_buff, 0, data_size);
-    
-    for (size_t i = 0; i < data_size; i++)
-    {
-        send_buff[i] = img->data[i];
-    }
-    
-    printf("image memory block:      \n"); memdump(img->data, 100);
+    memset(recv_buff, 0, DATA_SIZE);
+
+    fill_random(send_buff, DATA_SIZE);
+
     printf("Source memory block:      \n"); memdump(send_buff, 100);
     printf("Destination memory block: \n"); memdump(recv_buff, 100);
 
@@ -105,21 +131,53 @@ void test3()
     print_status(dma_mmio);
 
     printf("setting lengths...\n");
-    set_transfer_lengths(dma_mmio, data_size, data_size);
+    set_transfer_lengths(dma_mmio, DATA_SIZE, DATA_SIZE);
     print_status(dma_mmio);
 
     printf("waiting transfer's end...\n");
+    
+    clock_t hw_start, hw_end;
+    double hw_time;
+    hw_start = clock();
     wait_transfer(dma_mmio);
+    hw_end = clock();
+    hw_time = (hw_end - hw_start) / (CLOCKS_PER_SEC / (double) 1000.0);
+
     print_status(dma_mmio);
 
     printf("Source memory block:      \n"); memdump(send_buff, 100);
     printf("Destination memory block: \n"); memdump(recv_buff, 100);
+    
+    
+    clock_t sw_start, sw_end;
+    double sw_time;
+    sw_start = clock();
+    
+    for (size_t i = 0; i < DATA_SIZE; i++)
+    {
+        send_buff[i] = (send_buff[i] <= THRES) ? 0 : 255;
+    }
+    
+    sw_end = clock();
+    sw_time = (sw_end - sw_start) / (CLOCKS_PER_SEC / (double) 1000.0);
+    
+    printf("Source memory block:      \n"); memdump(send_buff, 100);
+    printf("Destination memory block: \n"); memdump(recv_buff, 100);
 
-    save_image(img,  "data/IP_threshold.pgm");
+    int check = compare_arrays(send_buff, recv_buff, DATA_SIZE);
+    
+    if(check)
+    {
+        printf("Software and hardware are equals !\n");
+    } else {
+        printf("Software and hardware are NOT equals !\n");
+    }
 
-    free_image(img);
-    free_mmio(recv_buff, data_size);
-    free_mmio(send_buff, data_size);
+    printf("Software took %f secs and hardware took %f secs\n", sw_time, hw_time);
+    printf("Hardware is %f times faster than Software\n", sw_time/hw_time);
+
+    free_mmio(recv_buff, DATA_SIZE);
+    free_mmio(send_buff, DATA_SIZE);
     free_mmio(dma_mmio, REGISTER_SIZE);
     free_mmio(dma_mmio, REGISTER_SIZE);
 }
